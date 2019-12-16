380c0c0a38322735f2cf680ef53eb19885b69b6c
chengbo <xuchengbo_poi@126.com>
2019-12-16 14:01:55 +0800

    还在公司摸鱼ing

--- main.py
+++ main.py
@@ -7,14 +7,24 @@ from task import Task
 taskArr = []
 commandArr = []
 with open('commandList.txt','r') as f:
-    txt = f.read().split("#")
-print txt
+    for line in f.readlines():
+        txt = line.strip().replace('\n','')
+        commandArr.append(txt)
 if len(sys.argv) == 2:
     if sys.argv[1] == "-d":
-        taskArr.append(Task("yum -y install java-1.8.0-openjdk"))
-        taskArr.append(Task("yum -y install java-1.8.0-openjdk"))
-        taskArr.append(Task("yum -y install java-1.8.0-openjdk"))
-
-        
+        for cmd in commandArr:
+                taskArr.append(Task(cmd))
+    if sys.argv[1] == "-b":
+        if os.path.exists('eula.txt'):
+                with open('eula.txt','w') as f:
+                        f.write('eula=true')
+                taskArr.append(Task("java -Xms512M -Xmx1024M -jar server.jar nogui"))
+        else:
+                taskArr.append(Task("java -Xms512M -Xmx1024M -jar server.jar nogui"))
+                with open('eula.txt','w') as f:
+                        f.write('eula=true')
+                taskArr.append(Task("java -Xms512M -Xmx1024M -jar server.jar nogui"))
+    if sys.argv[1] == "-h":
+        print '-d部署，-b启动'
 else:
     print("请输入正确的参数")
