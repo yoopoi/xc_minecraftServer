02d3ace06ac652c0ef757dd20c3754a1c51b9793
chengbo <xuchengbo_poi@126.com>
2019-12-16 10:34:09 +0800

    在公司摸鱼ing

--- main.py
+++ main.py
@@ -5,7 +5,13 @@ import sys
 import time
 from task import Task
 taskArr = []
+commandArr = []
+with open('commandList.txt','r') as f:
+    txt = f.read().split("#")
+print txt
 if len(sys.argv) == 2:
-    taskArr.append(Task("yum -y install java-1.8.0-openjdk"))
+    if sys.argv[1] == "-d":
+        taskArr.append(Task("yum -y install java-1.8.0-openjdk"))
+        
 else:
     print("请输入正确的参数")
